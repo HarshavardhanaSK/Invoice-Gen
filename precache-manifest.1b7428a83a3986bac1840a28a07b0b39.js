06ecefe92206c259c8eb2497c40b40465691c213
NISHANTH HARSHA 69 <nishanthharsha23@gmail.com>
2024-09-05 04:09:01 +0530

    Updates

---
+++ precache-manifest.1b7428a83a3986bac1840a28a07b0b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cbb416f57206a29958ea7569bbf945",
+    "url": "/Invoice-Gen/index.html"
+  },
+  {
+    "revision": "0bbe0d553791d545a65c",
+    "url": "/Invoice-Gen/static/css/main.562fec87.chunk.css"
+  },
+  {
+    "revision": "689a21f756ad91a146ac",
+    "url": "/Invoice-Gen/static/js/2.6a9c0ae6.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Invoice-Gen/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "0bbe0d553791d545a65c",
+    "url": "/Invoice-Gen/static/js/main.76c42288.chunk.js"
+  },
+  {
+    "revision": "bd53c96a5e3d20a666fe",
+    "url": "/Invoice-Gen/static/js/runtime~main.695558b7.js"
+  }
+]);
